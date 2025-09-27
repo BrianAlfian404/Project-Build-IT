d0d36b13203aa2d190151ead4450c3fec08ef618
BrianAlfian404 <brianalfiansyah260@gmail.com>
2025-09-27 11:21:59 +0800

    Create soal_3.c

---
+++ soal_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int A;
+    scanf("%d", &A);
+    int B;
+    scanf("%d", &B);
+    for(int i = 1; )
+    
+    // while(A<B){
+    //     A+=1;
+    // }
+    // printf("%d", A);
+        
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}

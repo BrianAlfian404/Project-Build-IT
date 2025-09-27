97f492903df6e50f3e86f9cd58ce375a12872949
BrianAlfian404 <brianalfiansyah260@gmail.com>
2025-09-27 11:28:58 +0800

    Create soal_1.c

---
+++ soal_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float jarak;
+    scanf("%f", &jarak);
+    float waktu;
+    scanf("%f", &waktu);
+    printf("%.3f", jarak/waktu);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
